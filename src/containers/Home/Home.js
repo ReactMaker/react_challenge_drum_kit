594573e9d439580746f284a7c8aef8296b7f20e5
Andy Tsai <andy@ketingdeMacbook-Pro.local>
2017-05-09 02:07:32 +0800

    detect keydown

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -3,11 +3,36 @@ import drums from './HomeConstants';
 import './Home.less';
 
 export default class Home extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      currentKeyCode: 0,
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.playSound);
+  }
+
+  playSound = (e) => {
+    const key = e.keyCode;
+    const matchDrum = drums.find(drum => drum.keyCode === key);
+
+    if (matchDrum && Object.keys(matchDrum).length !== 0) {
+      this.setState({
+        currentKeyCode: key,
+      });
+    }
+  }
+
   render() {
+    const { currentKeyCode } = this.state;
+
     return (
       <div className="keys">
         {drums.map(drum => (
-          <div className="key">
+          <div className={`key ${currentKeyCode === drum.keyCode ? 'playing' : ''}`}>
             <kbd>
               {drum.key}
             </kbd>
