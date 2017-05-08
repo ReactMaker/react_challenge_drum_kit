0bfd0ae5b2cce7e2bb45ec6a8600deb79c073033
Andy Tsai <andy@ketingdeMacbook-Pro.local>
2017-05-09 01:39:55 +0800

    html + css

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-
+import drums from './HomeConstants';
 import './Home.less';
 
 export default class Home extends Component {
   render() {
     return (
-      <div id="pageHome">
-        this is home page
+      <div className="keys">
+        {drums.map(drum => (
+          <div className="key">
+            <kbd>
+              {drum.key}
+            </kbd>
+            <span className="sound">
+              {drum.name}
+            </span>
+          </div>
+        ))}
       </div>
     );
   }
---
+++ src/containers/Home/HomeConstants.js
@@ -0,0 +1,33 @@
+export default [{
+  key: 'A',
+  keyCode: 65,
+  name: 'clap',
+}, {
+  key: 'S',
+  keyCode: 83,
+  name: 'hihat',
+}, {
+  key: 'F',
+  keyCode: 70,
+  name: 'openhat',
+}, {
+  key: 'G',
+  keyCode: 71,
+  name: 'boom',
+}, {
+  key: 'H',
+  keyCode: 72,
+  name: 'ride',
+}, {
+  key: 'J',
+  keyCode: 74,
+  name: 'snare',
+}, {
+  key: 'K',
+  keyCode: 75,
+  name: 'tom',
+}, {
+  key: 'L',
+  keyCode: 76,
+  name: 'tink',
+}];
